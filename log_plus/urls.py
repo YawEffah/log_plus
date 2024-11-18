b88955db7169c44288e3552999e4e2816f13bbba
YawEffah <ceffah012@gmail.com>
2024-11-18 14:00:38 +0000

    :Include log_manager urls

--- log_plus/urls.py
+++ log_plus/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from log_manager.views import checkin_view, checkout_view, index
 
 urlpatterns = [
+    path('', index, name='index'),
+    path('checkin', checkin_view, name='checkin'),
+    path('checkout', checkout_view, name='checkout'),
+    path('log_plus/', include('log_manager.urls')),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
