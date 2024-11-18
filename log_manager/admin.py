4c7eec93c9df259121bd2321abac323100afa325
YawEffah <ceffah012@gmail.com>
2024-11-18 14:06:09 +0000

    :Register custom models in admin.py

--- log_manager/admin.py
+++ log_manager/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import AttendanceRecord, Employee, Department
+
 # Register your models here.
+
+
+admin.site.register(AttendanceRecord)
+admin.site.register(Employee)
+admin.site.register(Department)
