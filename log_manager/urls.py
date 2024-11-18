f9d2ccd9468843554fe87928e54dcc43a2c3a11a
YawEffah <ceffah012@gmail.com>
2024-11-18 14:02:08 +0000

    :configure log_manager urls

---
+++ log_manager/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('employees/', employees_view, name='employees'),
+    path('statistics/', statistics_view, name='statistics'),
+    # path('add_employee/', add_employee_view, name='add_employee'),
+    path('edit_employee/<int:employee_id>/', edit_employee_view, name='edit_employee'),
+    path('delete_employee/<int:employee_id>/', delete_employee_view, name='delete_employee'),
+]
